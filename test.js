e6ba5b11a24ad484e4f4692be4aa924460e61c59
Landon Conover <landon.conover@gmail.com>
2015-03-04 08:45:06 -0700

    Removing code

--- test.js
+++ test.js
@@ -1,31 +1,3 @@
 var expect = require('expect');
 var math = require('./math.js');
 
-describe('addr', function () {
-    it('shoud add numbers', function () {
-        expect(math.adder(1,2)).toBe(1+2);
-    });
-
-    it('should add lots of numbers', function () {
-        expect(math.adder(1,2,3,4,5,6)).toBe(1+2+3+4+5+6);
-    });
-
-    it('should be a number', function () {
-        expect(math.adder(1,2)).toBeA('number')
-    });
-
-})
-
-describe('multiplyer', function () {
-    it('should multply numers', function () {
-        expect(math.multiplyer(1,2)).toBe(1 *2);
-    });
-
-    it('should multply lots of numers', function () {
-        expect(math.multiplyer(1,2,3,4,5,6)).toBe(1*2*3*4*5*6);
-    });
-
-    it('should be a number', function () {
-        expect(math.multiplyer(1,2)).toBeA('number')
-    });
-})
