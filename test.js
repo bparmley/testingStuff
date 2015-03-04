5b28f7b5155d7949af6a3dd40c2af5d489543954
Thor Anderson <ta.anderson@gmail.com>
2015-03-04 12:09:44 -0700

    Edited the ReadMe and added a little more stub code to test.js to get students started.

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
 var expect = require('expect');
 var math = require('./math.js');
 
+describe('adder', function () {
+    it('should add two arguments', function () {
+        expect(//something to add up here);
+    });
+
+    it('should be a number', function () {
+        expect(//something to be something here);
+    });
+
+})
