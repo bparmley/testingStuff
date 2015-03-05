559b204808e650319f1f9e93259b095f627f7a6c
bparmley <orem.bparmley@gmail.com>
2015-03-05 13:52:29 -0700

    split the describes into adder and multiplyer

--- test.js
+++ test.js
@@ -7,11 +7,12 @@ describe('adder', function () {
 	it('should be a number', function(){
 		expect(math.adder(2,3)).toBeA('number');
 	});
+})
+describe('multiplyer', function(){
 	it('should multiply two arguments', function(){
 		expect(math.multiplyer(2,3)).toBe(2*3);
 	});
     it('should be a number', function () {
         expect(math.multiplyer(2,3)).toBeA('number');
     });
-
 })
