af328080378062dc0bc75999ef3b77e1e24e2eff
bparmley <orem.bparmley@gmail.com>
2015-03-05 13:47:41 -0700

    passed the tests

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 var expect = require('expect');
 var math = require('./math.js');
-
 describe('adder', function () {
     it('should add two arguments', function () {
-        expect(//something to add up here);
+		expect(math.adder(2,3)).toBe(2+3);
     });
-
+	it('should be a number', function(){
+		expect(math.adder(2,3)).toBeA('number');
+	});
+	it('should multiply two arguments', function(){
+		expect(math.multiplyer(2,3)).toBe(2*3);
+	});
     it('should be a number', function () {
-        expect(//something to be something here);
+        expect(math.multiplyer(2,3)).toBeA('number');
     });
 
 })
